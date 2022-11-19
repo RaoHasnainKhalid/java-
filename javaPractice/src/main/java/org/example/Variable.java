d2912a7407edceb95aceee4509645849a29d65fe
Hasnain <hasnainrao786000@gmail.com>
2022-11-19 11:27:36 +0500

    Java Variable

--- javaPractice/src/main/java/org/example/Variable.java
+++ javaPractice/src/main/java/org/example/Variable.java
@@ -9,5 +9,9 @@ public class Variable {
         byte d = 100;
         int sum = a + b + c + d;
         System.out.println(sum);
+        float f = 162.5f;
+        double dbl = 5.7;
+        char name = 'H';
+        System.out.println(f + " "+ dbl  + " " + name);
     }
 }
