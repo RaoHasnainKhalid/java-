55dfeff989a11e83f074870c7f7adf09f18cd489
Hasnain <hasnainrao786000@gmail.com>
2022-11-19 11:22:11 +0500

    Java Variable

---
+++ javaPractice/src/main/java/org/example/Variable.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Variable {
+    public static void main(String[] args) {
+        System.out.print("The Sum Of Number: ");
+        byte a = 5;
+        byte b = 7;
+        byte c = 80;
+        byte d = 100;
+        int sum = a + b + c + d;
+        System.out.println(sum);
+    }
+}
