9b044f7bd7a2852897c726751710fa1deffbbb93
Hasnain <hasnainrao786000@gmail.com>
2022-11-20 12:52:50 +0500

    PrecedenceAndAssociativity

---
+++ javaPractice/src/main/java/org/example/PrecedenceAndAssociativity.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class PrecedenceAndAssociativity {
+    public static void main(String[] args) {
+        // Precedence
+        // (*,/) high  (+,-)low
+        int a = 5*6-4/2;
+        // output (28)
+        System.out.println(a);
+        // Associativity
+        // (/,*) left to right (+,-) left to right
+        int b = 8/4*2+4-2;
+        // output (6)
+        System.out.println(b);
+        
+
+    }
+}
