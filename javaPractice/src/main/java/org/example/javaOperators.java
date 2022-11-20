738cb503e9117d1e9efd72f56395b5d96befe4bb
Hasnain <hasnainrao786000@gmail.com>
2022-11-20 12:11:20 +0500

    Operator In Java

---
+++ javaPractice/src/main/java/org/example/javaOperators.java
@@ -0,0 +1,31 @@
+package org.example;
+import java.util.Scanner;
+public class javaOperators {
+    public static void main(String[] args) {
+        // Arithmatic operator
+//        int a = 5;
+//        int b = 7 + a;
+//        int c = 7 - b;
+//        int d = 7 * c;
+//        float e = 9f / d ;
+//        System.out.println(a + " " + b + " " +  c + " " + d + " " +e);
+//
+//        // Assignment Operator
+//        // = ,+= ,-=, *=, /=
+//        int x = 12;
+//        x += 9;
+//        System.out.println(x);
+        // Logical Operator
+        // & , || , !
+//        int a = 5;
+//        int b = 7;
+//        System.out.println(a>b &&  b>a);
+
+        // Comperison Operator
+        // == ,<,<,>=,<=
+        int a = 5;
+        int b = 7;
+        System.out.println(b>a);
+        System.out.println(a==b);
+    }
+}
